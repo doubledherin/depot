813d1b3580834252f0742bf22d4ef5c95f5f005b
Wendy Dherin <wendydherin@gmail.com>
2020-11-22 11:56:11 +0100

    Add migration to add product price to line item table.

---
+++ db/migrate/20201122101738_copy_product_price_to_line_item.rb
@@ -0,0 +1,8 @@
+class CopyProductPriceToLineItem < ActiveRecord::Migration[6.0]
+  def up
+    add_column :line_items, :price, :decimal, precision: 8, scale: 2
+  end
+  def down
+    remove_column :line_items, :price
+  end
+end
